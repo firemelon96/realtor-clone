137e9b928ee31ed961e1f95987acbceb095c7dd6
estong jamion <estong.jamion@gmail.com>
2022-11-15 09:01:15 +0800

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-900">Hello world!</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
